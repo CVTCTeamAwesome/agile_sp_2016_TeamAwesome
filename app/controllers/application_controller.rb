716b56ebac7c01ee6f5d8f18b383912b8d9b22e5
David VanSlamBrouck <spire2985@gmail.com>
2016-02-01 21:09:51 -0600

    Add Drop down box selector to Display Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_deck
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def set_deck
+      @decks = Deck.all
+  end
 end
