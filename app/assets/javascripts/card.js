6f34c2f0be431b1dedecc68c6b2ae84235bc53e2
David VanSlamBrouck <spire2985@gmail.com>
2016-02-03 02:56:41 -0600

    Add js to change cards
    Add javascript to change cards, remove one ajax form and it's struts

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,8 +1,40 @@
-$('#showAnswerButton').click(_toggleCardSide);
+var index = 0
+
+var _nextCard = function () {
+
+  index++;
+  var $cardDivs = $('.cardDiv');
+  $('.answerTextDiv').hide();
+  console.log(index);
+  if (index > $cardDivs.length - 1) {
+    index = 0;
+  }
+  $cardDivs.hide();
+  $cardDivs.eq(index).fadeIn();
+
+};
+
+var _previousCard = function () {
+
+  index--;
+  var $cardDivs = $('.cardDiv');
+  $('.answerTextDiv').hide();
+  console.log(index);
+  if (index < 0) {
+    index = $cardDivs.length - 1;
+  }
+  $cardDivs.hide();
+  $cardDivs.eq(index).fadeIn();
+
+};
 
 var _toggleCardSide = function () {
 
-$('#answerTextDiv').hide();
-$('.cardSide').toggle();
+  $('.cardSide').toggle();
 
 };
+
+$('.answerTextDiv').hide();
+$('#showAnswerButton').click(_toggleCardSide);
+$('#nextCardButton').click(_nextCard);
+$('#previousCardButton').click(_previousCard);
