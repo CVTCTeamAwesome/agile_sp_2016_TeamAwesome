a45ee993ce0bb26504e0bf8a4cbeaab5c685d727
David VanSlamBrouck <spire2985@gmail.com>
2016-02-03 10:45:51 -0600

    Add function to jump between cards

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,5 +1,18 @@
 var index = 0
 
+var _jumpToCard = function (e) {
+  
+  var clickedClass = $( this ).attr("class");
+  var $cardDivs = $('.cardDiv');
+  var $clickedCard = $('.cardDiv.' + clickedClass);
+  
+  e.preventDefault();
+  $cardDivs.hide();
+  $clickedCard.fadeIn();
+  index = $clickedCard.index();
+  
+};
+
 var _nextCard = function () {
 
   index++;
@@ -38,3 +51,4 @@ $('.answerTextDiv').hide();
 $('#showAnswerButton').click(_toggleCardSide);
 $('#nextCardButton').click(_nextCard);
 $('#previousCardButton').click(_previousCard);
+$('#render_partial_deck a').click(_jumpToCard);
