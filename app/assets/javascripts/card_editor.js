c7f630070ff06650c02f8fe545f1903be3a5b84f
Luke A Chase <chase.luke.a@gmail.com>
2016-02-03 16:32:30 -0600

    Add javascript to update text on cards as typed in field

---
+++ app/assets/javascripts/card_editor.js
@@ -0,0 +1,18 @@
+// Auto-update Card Editing page on attribute values changed
+$(document).ready(function () {
+  
+  var $questionText = $("textarea#card_question_text");
+  var $questionView = $("div.card_front p.card_text");
+  
+  $questionText.keyup(function () {
+    $questionView.html($questionText.val());
+  });
+  
+  var $answerText = $("textarea#card_answer_text");
+  var $answerView = $("div.card_back p.card_text");
+  
+  $answerText.keyup(function () {
+    $answerView.html($answerText.val());
+  });
+  
+});
