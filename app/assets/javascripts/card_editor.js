e2223e05ec55fd4df78f6c40e7d4b138e836e724
Luke A Chase <chase.luke.a@gmail.com>
2016-02-05 23:06:58 -0600

    Update card_editor so JavaScript works correctly with Turbolinks

--- app/assets/javascripts/card_editor.js
+++ app/assets/javascripts/card_editor.js
@@ -1,28 +1,17 @@
 // Auto-update Card Editing page on attribute values changed
-var $frontTextField = $("textarea#card_question_text");
-  var $cardFront = $("div.card_front");
-  var $cardFrontText = $("div.card_front p.card_text");
-  
-  var $backTextField = $("textarea#card_answer_text");
-  var $cardBack = $("div.card_back");
-  var $cardBackText = $("div.card_back p.card_text");
-  
-  var $foregroundColor = $("input#card_foreground_color");
-  var $backgroundColor = $("input#card_background_color");
-  
-  $foregroundColor.on("change", function () {
-    $cardFrontText.css("color", $foregroundColor.val());
-    $cardBackText.css("color", $foregroundColor.val());
-  });
-  
-  $backgroundColor.on("change", function () {
-    $cardFront.css("background", $backgroundColor.val());
-    $cardBack.css("background", $backgroundColor.val());
-  });
+$( document ).on("change", "input#card_foreground_color", function () {
+  $("div.card_front p.card_text").css("color", $( this ).val());
+  $("div.card_back p.card_text").css("color", $( this ).val());
+});
+
+$( document ).on("change", "input#card_background_color", function () {
+  $("div.card_front").css("background", $( this ).val());
+  $("div.card_back").css("background", $( this ).val());
+});
     
 $( document ).on ( "keyup", "textarea#card_question_text", function () {
     $("div.card_front p.card_text").html($( this ).val());
-  });
+});
 
 $( document ).on ( "keyup", "textarea#card_answer_text", function () {
     $("div.card_back p.card_text").html($( this ).val());
