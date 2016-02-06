a0cbba55b7c9b4340b699bca3a63f383e93a6f60
Luke A Chase <chase.luke.a@gmail.com>
2016-02-05 19:04:45 -0600

    Update card editor javascript so colors real-time update

--- app/assets/javascripts/card_editor.js
+++ app/assets/javascripts/card_editor.js
@@ -1,18 +1,34 @@
 // Auto-update Card Editing page on attribute values changed
 $(document).ready(function () {
   
-  var $questionText = $("textarea#card_question_text");
-  var $questionView = $("div.card_front p.card_text");
+  var $frontTextField = $("textarea#card_question_text");
+  var $cardFront = $("div.card_front");
+  var $cardFrontText = $("div.card_front p.card_text");
   
-  $questionText.keyup(function () {
-    $questionView.html($questionText.val());
+  var $backTextField = $("textarea#card_answer_text");
+  var $cardBack = $("div.card_back");
+  var $cardBackText = $("div.card_back p.card_text");
+  
+  var $foregroundColor = $("input#card_foreground_color");
+  var $backgroundColor = $("input#card_background_color");
+  
+  
+  $frontTextField.keyup(function () {
+    $cardFrontText.html($frontTextField.val());
   });
   
-  var $answerText = $("textarea#card_answer_text");
-  var $answerView = $("div.card_back p.card_text");
+  $backTextField.keyup(function () {
+    $cardBackText.html($backTextField.val());
+  });
   
-  $answerText.keyup(function () {
-    $answerView.html($answerText.val());
+  $foregroundColor.on("change", function () {
+    $cardFrontText.css("color", $foregroundColor.val());
+    $cardBackText.css("color", $foregroundColor.val());
+  });
+  
+  $backgroundColor.on("change", function () {
+    $cardFront.css("background", $backgroundColor.val());
+    $cardBack.css("background", $backgroundColor.val());
   });
   
 });
