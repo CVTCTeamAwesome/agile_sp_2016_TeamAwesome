8085cad8b1dc9b9449d23b928c3fd38f89215246
David VanSlamBrouck <spire2985@gmail.com>
2016-02-06 06:09:12 -0600

    Match current js files before merge

--- app/assets/javascripts/card_editor.js
+++ app/assets/javascripts/card_editor.js
@@ -16,13 +16,3 @@ $( document ).on ( "keyup", "textarea#card_question_text", function () {
 $( document ).on ( "keyup", "textarea#card_answer_text", function () {
     $("div.card_back p.card_text").html($( this ).val());
 });
-
-$( document ).on ( "change", "#card_background_color", function () {
-    $("div.card_back").css("color", $( this ).val());
-    $("div.card_front").css("color", $( this ).val());
-  });
-
-$( document ).on ( "change", "#card_foreground_color", function () {
-    $("div.card_back").css("color", $( this ).val());
-    $("div.card_front").css("color", $( this ).val());
-  });
