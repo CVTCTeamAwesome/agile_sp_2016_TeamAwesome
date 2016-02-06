642488df384602139ee558162217fe5d84e579d8
David VanSlamBrouck <spire2985@gmail.com>
2016-02-06 00:57:37 -0600

    bug fix card.rb and scheme

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
   
-  has_attached_file :picture,
+  has_attached_file :picture
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
