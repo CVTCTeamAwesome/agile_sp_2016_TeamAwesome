2e30f1d2da3a92594f3c4c0bf7e19732d0372155
David VanSlamBrouck <spire2985@gmail.com>
2016-02-07 01:20:47 -0600

    Add helper style to show pictures
    Added a helper to handle all of the styling for when a user adds a picture to a card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,40 @@
 module ApplicationHelper
 #Styles for when @card is used:
-  def picture_styles
+  def picture_style
     if @card.picture?
-      "background:#{@card.background_color} url(\"#{@card.picture.url}\") no-repeat center;background-size: cover;"
+      "background:#{@card.background_color} url(\"#{@card.picture.url}\") no-repeat center;background-size: cover;color: rgba(0,0,0,0.0);"
     else
       "background:#{@card.background_color};"
     end
     
   end
 
+#Styles for questionText
+  def questionText
+    if @card.picture?
+      "opacity:0;"
+    else
+      "color:#{@card.foreground_color};font-size:#{@card.font_size};font-style:#{@card.font_style};"
+    end
+  end
+  
+#Styles for questionText
+  def questionsText(card)
+    if card.picture?
+      "opacity:0;"
+    else
+      "color:#{card.foreground_color};font-size:#{card.font_size};font-style:#{card.font_style};"
+    end
+  end
+  
 #Styles for when |card| is used:
-  def card_picture_styles
-    #"background:#{@card.background_color} url(\"#{@card.picture.url}\") no-repeat center;background-size: cover;"
+  def picture_styles(card)
+    if card.picture?
+      "color:#{card.foreground_color};background:#{card.background_color} url(\"#{card.picture.url}\") no-repeat center;background-size: cover;color: rgba(0,0,0,0.0);"
+    else
+      "background:#{card.background_color};"
+    end
+    
   end
   
 end
