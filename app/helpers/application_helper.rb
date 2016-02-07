f5cb4ddfc29675743d84bd3df3be1c9ff0fa6f4a
David VanSlamBrouck <spire2985@gmail.com>
2016-02-07 00:44:17 -0600

    Add helper for stles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+#Styles for when @card is used:
+  def picture_styles
+    if @card.picture?
+      "background:#{@card.background_color} url(\"#{@card.picture.url}\") no-repeat center;background-size: cover;"
+    else
+      "background:#{@card.background_color};"
+    end
+    
+  end
+
+#Styles for when |card| is used:
+  def card_picture_styles
+    #"background:#{@card.background_color} url(\"#{@card.picture.url}\") no-repeat center;background-size: cover;"
+  end
+  
 end
