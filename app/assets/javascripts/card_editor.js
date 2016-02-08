c27938a1c6502206b0314d269a3c5d7821cef8bd
tsweitzer <tsweitzer@student.cvtc.edu>
2016-02-08 04:22:48 +0000

    Added javascript for font style and font size

--- app/assets/javascripts/card_editor.js
+++ app/assets/javascripts/card_editor.js
@@ -1,18 +1,69 @@
 // Auto-update Card Editing page on attribute values changed
-$(document).ready(function () {
-  
-  var $questionText = $("textarea#card_question_text");
-  var $questionView = $("div.card_front p.card_text");
-  
-  $questionText.keyup(function () {
-    $questionView.html($questionText.val());
-  });
-  
-  var $answerText = $("textarea#card_answer_text");
-  var $answerView = $("div.card_back p.card_text");
-  
-  $answerText.keyup(function () {
-    $answerView.html($answerText.val());
-  });
-  
+$( document ).on("change", "input#card_foreground_color", function () {
+  $("div.card_front p.card_text").css("color", $( this ).val());
+  $("div.card_back p.card_text").css("color", $( this ).val());
 });
+
+$( document ).on("change", "input#card_background_color", function () {
+  $("div.card_front").css("background", $( this ).val());
+  $("div.card_back").css("background", $( this ).val());
+});
+
+$( document ).on("change", "card_font_style", function () {
+ $("div.card_front").css("font-style", $( this ).val());
+ $("div.card_back").css("font-style", $( this ).val());
+});
+
+$( document ).on("change", "card_font_size", function () {
+ $("div.card_front").css("font-size", $( this ).val());
+ $("div.card_back").css("font-size", $( this ).val());
+});
+
+// Auto-update Card Editing page on attribute values changed
+$( document ).on("change", "input#card_foreground_color", function () {
+  $("div.card_front p.card_text").css("color", $( this ).val());
+  $("div.card_back p.card_text").css("color", $( this ).val());
+});
+
+$( document ).on("change", "input#card_background_color", function () {
+  $("div.card_front").css("background", $( this ).val());
+  $("div.card_back").css("background", $( this ).val());
+});
+    
+$( document ).on ( "keyup", "textarea#card_question_text", function () {
+    $("div.card_front p.card_text").html($( this ).val());
+});
+
+$( document ).on ( "keyup", "textarea#card_answer_text", function () {
+    $("div.card_back p.card_text").html($( this ).val());
+});
+
+// code to update image after one selected, not working right now due
+// to inability to access the full file path; looking into this
+//$( document ).on ( "change", "input#card_picture", function () {
+//  var image = $( this ).val();
+//  image = image.replace("C:\\fakepath\\", "");
+//  $("div.card_front").css({
+//    "background" : "url(\"" + image + "\") no-repeat center",
+//    "background-size" : "contain"
+//  });
+//});
+    
+$( document ).on ( "keyup", "textarea#card_question_text", function () {
+    $("div.card_front p.card_text").html($( this ).val());
+});
+
+$( document ).on ( "keyup", "textarea#card_answer_text", function () {
+    $("div.card_back p.card_text").html($( this ).val());
+});
+
+// code to update image after one selected, not working right now due
+// to inability to access the full file path; looking into this
+//$( document ).on ( "change", "input#card_picture", function () {
+//  var image = $( this ).val();
+//  image = image.replace("C:\\fakepath\\", "");
+//  $("div.card_front").css({
+//    "background" : "url(\"" + image + "\") no-repeat center",
+//    "background-size" : "contain"
+//  });
+//});
