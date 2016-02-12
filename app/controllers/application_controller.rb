40d1fc08a1d5dbf92073d5c442110ae4c51e073e
David VanSlamBrouck <spire2985@gmail.com>
2016-02-12 12:13:08 -0600

    Adjust authenticate for only new,edit, delete actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_action :authenticate, :except => [:index]
+  before_action :authenticate, :only => [:new, :edit, :destroy]
   
   
   def authenticate
