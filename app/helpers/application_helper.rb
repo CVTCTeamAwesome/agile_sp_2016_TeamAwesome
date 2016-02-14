169bc7f4c3f0c5f9cb133584fd1b7c111cd31855
David VanSlamBrouck <spire2985@gmail.com>
2016-02-14 15:54:22 -0600

    Adjust picture and text for caption display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,21 +24,22 @@ module ApplicationHelper
 #Styles for questionText
   def questionsText(card)
     if card.picture?
-      "opacity:0;"
-    else
-      "color:#{card.foreground_color};font-family:#{card.font_style};"
+      "background-color:#{card.background_color};
+       top: 75%;"
     end
   end
   
 #Styles for when |card| is used:
   def picture_styles(card)
-    if card.picture?
-      "color:#{card.foreground_color};background-color:#{card.background_color};
+    if card.picture? && card.question_text.present?
+      "color:#{card.foreground_color};font-size:#{card.font_size};font-family:#{card.font_style};
+       background-color:#{card.background_color};
        background-image: url(\"#{card.picture.url}\");
        background-repeat: no-repeat;
-       background-position: center;background-size: contain;color: rgba(0,0,0,0.0);"
+       background-position: center;background-size: contain;"
     else
-      "background-color:#{card.background_color};"
+      "background-color:#{card.background_color};
+       color:#{card.foreground_color};font-size:#{card.font_size};font-family:#{card.font_style};"
     end
     
   end
