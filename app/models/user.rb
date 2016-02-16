4800cc746ea8e75aea2b5a7b9cce489c3c82902e
= <jtstodola@gmail.com>
2016-02-15 19:19:07 -0600

    Completed db:migrate and adjusted user model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  before_action :authenticate_user!
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  # user_signed_in? - verify a user is signed in
+  # current_user    - for the current signed-in user
+  # user_session    - accessing the session for this scope
+  
+end
