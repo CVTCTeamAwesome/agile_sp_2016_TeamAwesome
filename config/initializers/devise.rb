f9c31ea788cfbfc0419094ec4fe1e0130b83bf03
David VanSlamBrouck <spire2985@gmail.com>
2016-02-23 19:57:05 -0600

    Add more google plus specific authorization

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,10 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+  
   config.omniauth :facebook, ENV["APP_ID"], ENV["APP_SECRET"]
-  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"]
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
+    :client_options => {:ssl => {:ca_path => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 
 end
