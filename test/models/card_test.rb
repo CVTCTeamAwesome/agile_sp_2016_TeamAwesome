17db1ea35fee7d209ca04f04bc0c8f5613b39bfe
David VanSlamBrouck <spire2985@gmail.com>
2016-02-23 22:20:13 -0600

    Add test to show you can attach a picture
    Add a test to show that you can attach a picture to a card.  Also fixed the test environment so it also has S3 information.

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -18,4 +18,13 @@ class CardTest < ActiveSupport::TestCase
     assert card.save
   end
   
+  test "can create card with picture" do
+    card = Card.new
+    card.name = "Test Card2"
+    card.answer_text = "Answer Text"
+    card.question_text = "This is question_text"
+    card.picture = File.new('test/models/ineedit.jpg')
+    assert card.save
+  end
+  
 end
