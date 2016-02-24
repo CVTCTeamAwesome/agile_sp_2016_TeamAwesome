4f7b8ff8395bc0ac5987e1e44b8bd9a83b549e18
David VanSlamBrouck <spire2985@gmail.com>
2016-02-23 22:33:11 -0600

    Add validation to test pass
    Add validation to make picture instead of question_text pass

--- app/models/card.rb
+++ app/models/card.rb
@@ -10,5 +10,6 @@ class Card < ActiveRecord::Base
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   
   validates_presence_of :name, :answer_text
-  validates_presence_of :question_text
+  validates_presence_of :question_text, presence: true, unless: -> (card){card.picture.present?}
+  
 end
