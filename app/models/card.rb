0bd0a5ce365b166dc746bb37206c49d1a29e9462
David VanSlamBrouck <spire2985@gmail.com>
2016-02-23 21:35:40 -0600

    Implement fix for card test, add message to test

--- app/models/card.rb
+++ app/models/card.rb
@@ -10,4 +10,5 @@ class Card < ActiveRecord::Base
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   
   validates_presence_of :name, :answer_text
+  validates_presence_of :question_text
 end
--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -6,7 +6,8 @@ class CardTest < ActiveSupport::TestCase
     card = Card.new
     card.name = "Test Card"
     card.answer_text = "Answer Text"
-    assert card.save  # This 'should' fail as Question_Text should be required.  Change after fix implemented.
+    assert !card.save  # This 'should' fail as Question_Text should be required.  Change after fix implemented.
+    assert card.errors[:question_text].include?("can't be blank")
   end
   
 end
